fa815bbe810988d0f03f4ae5ef77b31324510470
acheronii <Jrchurch02@gmail.com>
2025-06-05 01:22:37 -0400

    Added reset board button

--- hexchess_django/game/urls.py
+++ hexchess_django/game/urls.py
@@ -6,4 +6,6 @@ app_name = "game"
 urlpatterns = [
     path('', views.board_view, name='game_page'),
     path('click/', views.on_click, name='click'),
+    path('reset/', views.reset_board, name='reset'),
+    path('flip/', views.flip_board, name='flip'),
 ]
--- hexchess_django/game/views.py
+++ hexchess_django/game/views.py
@@ -23,36 +23,12 @@ def on_click(request):
 
     tile_id = request.POST.get('tile_id').split()
     board.on_click(int(tile_id[0]), int(tile_id[1]))
-    template = loader.get_template("game/board.html")
-    context = {"board": board.as_json(),
-               "turn": "White" if board.turn == 0 else "Black"
-               }
-    return HttpResponse(template.render(context, request))
+    return HttpResponseRedirect(reverse("game:game_page"))
 
+def reset_board(request):
+    board.__init__()
+    return HttpResponseRedirect(reverse("game:game_page"))
 
-def move_piece(request):    
-    template = loader.get_template("game/board.html")
-    
-    # Extract the actual coordinates 
-    # TODO: change how this is done when i change the input format
-    
-    try: 
-        from_str = request.POST['Hex_from']
-        to_str = request.POST['Hex_to']
-    except:
-        context = {"board": board.as_json(), 
-                   "error_message": "Please select a move"}
-        return HttpResponse(template.render(context, request))
-    
-    from_q, from_r = from_str.split(" ")
-    from_hex = (int(from_q), int(from_r))
-    to_q, to_r = to_str.split(" ")
-    to_hex = (int(to_q), int(to_r))
-
-    if (not board.move_piece(from_hex, to_hex)):
-        context = {"board": board.as_json(), 
-                   "error_message": "Illegal Move"}
-        return HttpResponse(template.render(context, request))
-
-    context = {"board": board.as_json(), "change_message": f"Moved from {from_hex} to {to_hex}"}
-    return HttpResponse(template.render(context, request))
+def flip_board(request):
+    board.flip()
+    return HttpResponseRedirect(reverse("game:game_page"))
