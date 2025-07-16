51459905ada6269618d7ba0eeb4924a1083040d6
= <Jrchurch02@gmail.com>
2025-07-16 01:22:19 -0400

    Feature (Client side flipping): added flipping on the client's side so it doesnt need a page reload not to bother the server to flip (dont ask me how it works, i did this at 1 am)

--- apps/game/consumers.py
+++ apps/game/consumers.py
@@ -3,7 +3,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.serializers.json import DjangoJSONEncoder
 
 from apps.game.state import board
-# from . import board
 
 
 class GameConsumer(AsyncWebsocketConsumer):
@@ -23,69 +22,49 @@ class GameConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
 
-        move = data.get("move")
-        player = self.scope["user"].username
-        print(f"[{self.room_group_name}]: Move {move} made by {player}")
-        """
-        TODO add validation for player and move, form payload to send to the javascript
-        something like :
-            # Check that the player is the correct player
-            current_turn = board.turn
-            if (current_turn == 0 and user != room.white_player) or \
-                (current_turn == 1 and user != room.black_player):
-                    # Not this user's turn — reject
-                    await self.send(text_data=json.dumps({
-                        "error": "You are not allowed to make this move."
-                    }))
-                    return
+        message_type = data.get("type")
 
-            # 1. Apply the move (this modifies your game state on the backend)
-            # look at views.ajax_move_view to copy for this
-            board.move_piece(from_pos, to_pos)
-            board.next_turn()
+        if message_type == "move":
+            move = data.get("move")
+            player = self.scope["user"].username
+            print(f"[{self.room_group_name}]: Move {move} made by {player}")
+            """
+            TODO add validation for player and move, form payload to send to the javascript
+            something like :
+                # Check that the player is the correct player
+                current_turn = board.turn
+                if (current_turn == 0 and user != room.white_player) or \
+                    (current_turn == 1 and user != room.black_player):
+                        # Not this user's turn — reject
+                        await self.send(text_data=json.dumps({
+                            "error": "You are not allowed to make this move."
+                        }))
+                        return
+            """
 
-            # 2. Prepare all state you want the clients to know
-            response = {
-                "move": move_str, # for displaying the change
-                "turn": "White" if board.turn == 0 else "Black",
-                "winner": board.winner,
-                "legal_moves_json": json.dumps(board.legal_moves, cls=DjangoJSONEncoder),
-                # optionally, board diff or full board state
-            }
-
-            # 3. Broadcast to all clients in room
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "broadcast_move",
-                    "payload": response
+            fro, to = move.split(">")
+            if fro in board.legal_moves.keys() and to in board.legal_moves[fro]:
+                # convert locations to ints
+                fro = [int(x) for x in fro.split("_")]
+                to = [int(x) for x in to.split("_")]
+                # move the piece
+                board.move_piece((int(fro[0]), int(fro[1])), (int(to[0]), int(to[1])))
+                board.next_turn()
+                response = {
+                    "move": move,
+                    "turn": "White" if board.turn == 0 else "Black",
+                    "winner": board.winner,
+                    "check": board.check,
+                    "legal_moves_json": json.dumps(
+                        board.legal_moves, cls=DjangoJSONEncoder
+                    ),
                 }
-
-        """
-
-        fro, to = move.split(">")
-        if fro in board.legal_moves.keys() and to in board.legal_moves[fro]:
-            # convert locations to ints
-            fro = [int(x) for x in fro.split("_")]
-            to = [int(x) for x in to.split("_")]
-            # move the piece
-            board.move_piece((int(fro[0]), int(fro[1])), (int(to[0]), int(to[1])))
-            board.next_turn()
-            response = {
-                "move": move,
-                "turn": "White" if board.turn == 0 else "Black",
-                "winner": board.winner,
-                "check": board.check,
-                "legal_moves_json": json.dumps(
-                    board.legal_moves, cls=DjangoJSONEncoder
-                ),
-            }
-        else:
-            response = {"reload": "True"}
-        # response = {"move": move}
-        await self.channel_layer.group_send(
-            self.room_group_name, {"type": "broadcast_move", "data": response}
-        )
+            else:
+                response = {"reload": "True"}
+            # response = {"move": move}
+            await self.channel_layer.group_send(
+                self.room_group_name, {"type": "broadcast_move", "data": response}
+            )
 
     async def broadcast_move(self, event):
         await self.send(text_data=json.dumps(event["data"]))
